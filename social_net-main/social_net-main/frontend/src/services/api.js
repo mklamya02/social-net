301c781c35a29a128efa66f801500f5f34dcff39
MK_lamya <lmlamya02@gmail.com>
2026-01-22 17:22:32 +0100

    Update api.js

--- social_net-main/social_net-main/frontend/src/services/api.js
+++ social_net-main/social_net-main/frontend/src/services/api.js
@@ -20,7 +20,7 @@ import axios from 'axios';
  */
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api', // Backend API URL
+  baseURL: "https://backend-production-926f.up.railway.app/api" || 'http://localhost:8080/api', // Backend API URL
   withCredentials: true, // CRITICAL: Allows sending/receiving cookies (for refresh token)
   headers: {
     // Let axios set Content-Type automatically based on the request body
